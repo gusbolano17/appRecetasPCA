e064bb73898c87b4edba045d55e5aa598bd62a61
gusbolano17 <gustavoadolfobolano@gmail.com>
2025-01-13 19:51:32 -0500

    primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appRecetas',
+  webDir: 'www'
+};
+
+export default config;
